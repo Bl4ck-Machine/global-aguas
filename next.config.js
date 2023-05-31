fcabc38b10bee1ef9f46f6c9e0db07a095f55383
= <=>
2023-05-31 20:07:15 +0100

    Fix congif

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'demos.creative-tim.com',
+          port: '',
+          pathname: '/corporate-ui-dashboard/assets/img/**',
+        },
+      ],
+    },
+  }
 
 module.exports = nextConfig
