83bd6dc7929b121335b9c2e18b2fa3199cf7965d
= <=>
2023-05-31 20:09:02 +0100

    main page layout done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,134 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        
+         
+
+          {/** Content */}
+        <main className="flex-1 p-6">
+          
+          {/** Navbar */}
+          <div className="flex flex-row">
+            <div className="flex-1">
+              <div className="font-bold">Dashboard</div>
+            </div>
+            <div className="pr-4">Notification</div>
+            <div className="pr-4">Definition</div>
+            <div className="pr-4">Account</div>
+          </div>
+
+          {/** Content */}
+          <div className="mt-32">
+            <div className="flex flex-wrap -mx-3">
+             
+              <div className="w-full max-w-full px-3 sm:w-1/2 sm:flex-none xl:w-1/4">
+                <div className="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+                  <div className="flex-auto p-4">
+                    <div className="flex flex-row -mx-3">
+                      <div className="flex-none w-2/3 max-w-full px-3">
+                        <div>
+                          <p className="mb-0 font-sans font-semibold leading-normal text-sm">
+                            Sales
+                          </p>
+                          <h5 className="mb-0 font-bold">
+                            $103,430
+                            <span className="leading-normal text-sm font-weight-bolder text-lime-500">
+                              +5%
+                            </span>
+                          </h5>
+                        </div>
+                      </div>
+                      <div className="px-3 text-right basis-1/3">
+                        <div className="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+                          <i className="ni leading-none ni-cart text-lg relative top-3.5 text-white"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+
+            <div className="flex flex-wrap mt-6 -mx-3">
+              
+              <div className="w-full px-3 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
+                <div className="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+                  <div className="flex-auto p-4">
+                    <div className="flex flex-wrap -mx-3">
+                      <div className="max-w-full px-3 lg:w-1/2 lg:flex-none">
+                        <div className="flex flex-col h-full">
+                          <p className="pt-2 mb-1 font-semibold">
+                            Built by developers
+                          </p>
+                          <h5 className="font-bold">Soft UI Dashboard</h5>
+                          <p className="mb-12">
+                            From colors, cards, typography to complex elements,
+                            you will find the full documentation.
+                          </p>
+                          <a
+                            className="mt-auto mb-0 font-semibold leading-normal text-sm group text-slate-500"
+                            href="javascript:;"
+                          >
+                            Read More
+                            <i
+                              className="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"
+                              aria-hidden="true"
+                            ></i>
+                          </a>
+                        </div>
+                      </div>
+                      <div className="max-w-full px-3 mt-12 ml-auto text-center lg:mt-0 lg:w-5/12 lg:flex-none">
+                        <div className="h-full bg-gradient-to-tl from-purple-700 to-pink-500 rounded-xl">
+                          {/* <img src="../assets/img/shapes/waves-white.svg" className="absolute top-0 hidden w-1/2 h-full lg:block" alt="waves"> */}
+                          <div className="relative flex items-center justify-center h-full">
+                            {/* <img className="relative z-20 w-full pt-6" src="../assets/img/illustrations/rocket-white.png" alt="rocket"> */}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
+                <div className="border-black/12.5 shadow-soft-xl relative flex h-full min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border p-4">
+                  <div className="relative h-full overflow-hidden bg-cover rounded-xl">
+                    <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-gray-900 to-slate-800 opacity-80"></span>
+                    <div className="relative z-10 flex flex-col flex-auto h-full p-4">
+                      <h5 className="pt-2 mb-6 font-bold text-white">
+                        Work with the rockets
+                      </h5>
+                      <p className="text-white">
+                        Wealth creation is an evolutionarily recent positive-sum
+                        game. It is all about who take the opportunity first.
+                      </p>
+                      <a
+                        className="mt-auto mb-0 font-semibold leading-normal text-white group text-sm"
+                        href="javascript:;"
+                      >
+                        Read More
+                        <i
+                          className="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"
+                          aria-hidden="true"
+                        ></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </main>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="bg-zinc-880 border-zinc-700 px-6">footer</footer>
+    </div>
+  );
 }
